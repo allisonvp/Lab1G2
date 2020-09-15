008fcb6996440f3411ae4cf07b1f8050d813e503
allisonvp <a.velardep@pucp.edu.pe>
2020-09-15 08:51:39 -0500

    entidades y herencia

---
+++ src/pe/pucp/edu/telecom/manager/DirectorioTelefonico.java
@@ -0,0 +1,15 @@
+package pe.pucp.edu.telecom.manager;
+
+import pe.pucp.edu.telecom.entity;
+
+import java.util.ArrayList;
+
+public class DirectorioTelefonico {
+
+    public String agregar() {
+
+
+        ArrayList<Usuario> arrayList = new ArrayList<>();
+        return "";
+    }
+}
